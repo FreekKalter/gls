e725df36fa8b7530ba9f0a6f14db2ba40c9056e1
Freek Kalter <freek@kalteronline.org>
2013-07-24 19:43:09 +0200

    lots and lots
    * added options
    - sort by state (default is name)
    - show only dirty dirs (this is fast, and probably most useful)
    
    * little refactoring on the way gls handles Projects, to enable a more
    detailed listing when -l is pecified
     - option also found in ls : modification date, permissions, etc..
     - Ideas ( last commit message in dir, which branch it is on )

--- gls.go
+++ gls.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/FreekKalter/text/columnswriter"
+	"github.com/FreekKalter/text/tabwriter"
 	"github.com/str1ngs/ansi/color"
 	"os"
 	"os/exec"
@@ -19,6 +20,7 @@ var wg sync.WaitGroup
 type colorFunc func(interface{}) *color.Escape
 
 var colorMap map[string]colorFunc = map[string]colorFunc{
+	"ok":                 func(i interface{}) *color.Escape { return color.BgDefault(color.Default(i)) },
 	"no_version_control": func(i interface{}) *color.Escape { return color.Bold(color.Blue(i)) },                  // Blue
 	"dirty":              func(i interface{}) *color.Escape { return color.Bold(color.Red(i)) },                   // Red
 	"no_remote":          func(i interface{}) *color.Escape { return color.BgBlue(color.Bold(color.Red(i))) },     // Red on Blue
@@ -27,9 +29,12 @@ var colorMap map[string]colorFunc = map[string]colorFunc{
 	"branch_behind":      func(i interface{}) *color.Escape { return color.BgYellow(color.Bold(color.Red(i))) },   // Red on Yellow
 }
 
+var TimeFormat = "Jan 2,2006 15:04"
+
 // Struct returned by gls go-routines
 type Project struct {
 	Name, State string
+	Info        os.FileInfo
 }
 
 type Projects []*Project
@@ -43,23 +48,32 @@ func (s ByName) Less(i, j int) bool {
 	return strings.ToLower(s.Projects[i].Name) < strings.ToLower(s.Projects[j].Name)
 }
 
+type ByState struct{ Projects }
+
+var sortOrderStates = map[string]int{"ok": 0, "no_version_control": 1, "dirty": 2, "no_remote": 3, "fetch_failed": 4, "branch_ahead": 5, "branch_behind": 6}
+
+func (s ByState) Less(i, j int) bool {
+	return sortOrderStates[s.Projects[i].State] < sortOrderStates[s.Projects[j].State]
+}
+
 var (
 	cleanGitRegex = regexp.MustCompile("nothing to commit")
 	fetchErrors   = regexp.MustCompile("^fatal")
 	branchAhead   = regexp.MustCompile("branch is ahead of")
 	branchBehind  = regexp.MustCompile("branch is behind")
 )
+var help, list, onlyDirty, sortByState bool
 
 func main() {
-	var help, list bool
-	//TODO: Add sort by name or state
 	flag.BoolVar(&help, "help", false, "print help message")
 	flag.BoolVar(&list, "l", false, "display results in 1 long list")
+	flag.BoolVar(&onlyDirty, "dirty", false, "only show diry dirs, this is very fast because it does not check remotes")
+	flag.BoolVar(&sortByState, "statesort", false, "sort output by state")
 	flag.Parse()
 	if help {
-        fmt.Println("Options:")
-        fmt.Println("-l:   use a longlisting format (like ls -l)\n")
-        fmt.Println("Color codes:")
+		flag.Usage()
+		fmt.Println("")
+		fmt.Println("Color codes:")
 		for k, v := range colorMap {
 			fmt.Println(v(k))
 		}
@@ -69,16 +83,16 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	glsResults := make(chan Project, 1000)
+	glsResults := make(chan *Project, 1000)
 
 	var projects Projects
 	for _, file := range files {
 		file_info, _ := os.Stat(file)
 		if file_info.IsDir() {
 			wg.Add(1)
-			go gls(file, glsResults)
+			go gls(&Project{Name: file, Info: file_info}, glsResults)
 		} else {
-			projects = append(projects, &Project{Name: file, State: "ok"})
+			projects = append(projects, &Project{Name: file, State: "ok", Info: file_info})
 		}
 	}
 	wg.Wait()
@@ -88,27 +102,26 @@ func main() {
 		// make a copy to add to []projects, because res always points to the same address space
 		toAppend := res
 		toAppend.Name = filepath.Base(res.Name)
-		projects = append(projects, &toAppend)
+		projects = append(projects, toAppend)
 	}
-	sort.Sort(ByName{projects})
+	if sortByState {
+		sort.Sort(ByState{projects})
+	} else {
+		sort.Sort(ByName{projects})
+	}
 
 	if list {
+		w := new(tabwriter.Writer)
+		w.Init(os.Stdout, 0, 8, 0, '\t', tabwriter.StripEscape)
 		for _, p := range projects {
-			if p.State == "ok" {
-				fmt.Printf("%s\n", p.Name)
-			} else {
-				fmt.Printf("%s\n", colorMap[p.State](p.Name))
-			}
+			fmt.Fprintf(w, "%s\t%s\t%s\n", colorMap[p.State](p.Name), humanReadable(p.Info.Size()), p.Info.ModTime().Format(TimeFormat))
 		}
+		w.Flush()
 	} else {
 
 		var projectsString string
 		for _, p := range projects {
-			if p.State == "ok" {
-				projectsString = fmt.Sprintf("%s\t%s", projectsString, p.Name)
-			} else {
-				projectsString = fmt.Sprintf("%s\t%s", projectsString, colorMap[p.State](p.Name))
-			}
+			projectsString = fmt.Sprintf("%s\t%s", projectsString, colorMap[p.State](p.Name))
 		}
 
 		w := columnswriter.New(os.Stdout, '\t', 0, 2)
@@ -117,27 +130,30 @@ func main() {
 	}
 }
 
-func gls(dirName string, result chan Project) {
+func gls(project *Project /*dirName string*/, result chan *Project) {
 	defer wg.Done()
-	var ret Project = Project{Name: dirName}
 
 	// First chek, is the directory under (git) version control
-	if ok, _ := exists(filepath.Join(dirName, ".git")); !ok {
-		ret.State = "no_version_control"
-		result <- ret
+	if ok, _ := exists(filepath.Join(project.Name, ".git")); !ok {
+		if !onlyDirty {
+			project.State = "no_version_control"
+			result <- project
+		}
 		return
 	}
 
-	gitDir := fmt.Sprintf("--git-dir=%s", filepath.Join(dirName, ".git"))
-	gitTree := fmt.Sprintf("--work-tree=%s", dirName)
+	gitDir := fmt.Sprintf("--git-dir=%s", filepath.Join(project.Name, ".git"))
+	gitTree := fmt.Sprintf("--work-tree=%s", project.Name)
 	output, err := exec.Command("git", gitDir, gitTree, "status").Output() //, gitDir, gitTree, "status")
 	if err != nil {
 		panic(err)
 	}
 	// Are there uncommitted changes is the directory (dirty)
 	if !cleanGitRegex.MatchString(strings.TrimSpace(string(output))) {
-		ret.State = "dirty"
-		result <- ret
+		project.State = "dirty"
+		result <- project
+		return
+	} else if onlyDirty {
 		return
 	}
 
@@ -147,22 +163,22 @@ func gls(dirName string, result chan Project) {
 		panic(err)
 	}
 	if len(output) == 0 {
-		ret.State = "no_remote"
-		result <- ret
+		project.State = "no_remote"
+		result <- project
 		return
 	}
 
 	// Fetch latest changes from remote
 	output, err = exec.Command("git", gitDir, gitTree, "fetch").Output()
 	if err != nil {
-		ret.State = "fetch_failed"
-		result <- ret
+		project.State = "fetch_failed"
+		result <- project
 		return
 	}
 	outputStr := strings.TrimSpace(string(output))
 	if fetchErrors.MatchString(outputStr) {
-		ret.State = "fetch_failed"
-		result <- ret
+		project.State = "fetch_failed"
+		result <- project
 		return
 	}
 
@@ -174,17 +190,17 @@ func gls(dirName string, result chan Project) {
 
 	// Is branch ahead of behind of remote
 	if branchAhead.MatchString(outputStr) {
-		ret.State = "branch_ahead"
-		result <- ret
+		project.State = "branch_ahead"
+		result <- project
 		return
 	} else if branchBehind.MatchString(outputStr) {
-		ret.State = "branch_behind"
-		result <- ret
+		project.State = "branch_behind"
+		result <- project
 		return
 	}
 
-	ret.State = "ok"
-	result <- ret
+	project.State = "ok"
+	result <- project
 }
 
 func exists(path string) (bool, error) {
@@ -196,4 +212,15 @@ func exists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+func humanReadable(filesize int64) string {
+	fs := float64(filesize)
+	for _, x := range []string{"b", "kb", "mb", "gb", "tb"} {
+		if fs < 1024 {
+			return fmt.Sprintf("%3.1f % s", fs, x)
+		}
+		fs /= 1024
+	}
+	return ""
 }
